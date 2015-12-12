ad7ca51749042bbd12f6e2284e69cb6650d4f3fa
Darshan Thaker <dbthaker@gmail.com>
2015-12-12 04:29:33 -0600

    new test script that outputs total times

--- test/test.sh
+++ test/test.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
 cd ~/multiboost/build
-cmake ../
-make
+cmake ../ >/dev/null
+make >/dev/null 2>&1
 cd ~/multiboostOLD/build
-cmake ../
-make
+cmake ../ >/dev/null
+make >/dev/null 2>&1
 
 cd ~/multiboost/test
 
-~/multiboost/build/multiboost --stronglearner AdaBoostPL --nworkers 7 --nworkers2 3 --fileformat arff --train pendigitsTrain.arff 200 --verbose 0 --learnertype SingleStumpLearner --outputinfo resultsSingleStump.dta --shypname shypSingleStump.xml 
 
-~/multiboost/build/multiboost --stronglearner AdaBoostPL --nworkers 7 --nworkers2 3 --fileformat arff --cmatrix pendigitsTest.arff shypSingleStump0.xml OUTPUT.txt
+NEWTRAIN=$(~/multiboost/build/multiboost --stronglearner AdaBoostPL --nworkers 7 --nworkers2 3 --fileformat arff --train pendigitsTrain.arff 200 --verbose 0 --learnertype SingleStumpLearner --outputinfo resultsSingleStump.dta --shypname shypSingleStump.xml | grep "Training time" )
 
-echo "-------------------------------------------------------------"
+NEWTEST=$(~/multiboost/build/multiboost --stronglearner AdaBoostPL --nworkers 7 --nworkers2 3 --fileformat arff --cmatrix pendigitsTest.arff shypSingleStump0.xml OUTPUT.txt | grep "Testing time")
 
-~/multiboostOLD/build/multiboost --fileformat arff --train pendigitsTrain.arff 200 --verbose 0 --learnertype SingleStumpLearner --outputinfo resultsSingleStumpOLD.dta --shypname shypSingleStumpOLD.xml
-~/multiboostOLD/build/multiboost --fileformat arff --cmatrix pendigitsTest.arff shypSingleStumpOLD.xml OUTPUTOLD.txt
+TRAIN=$(echo $NEWTRAIN | awk  '{print $NF}')
+TEST=$(echo $NEWTEST | awk '{print $NF}')
+echo "NEW TIMES:"
+python -c "print $TRAIN + $TEST"
+
+
+OLDTRAIN=$(~/multiboostOLD/build/multiboost --fileformat arff --train pendigitsTrain.arff 200 --verbose 0 --learnertype SingleStumpLearner --outputinfo resultsSingleStumpOLD.dta --shypname shypSingleStumpOLD.xml | grep "Training time")
+OLDTEST=$(~/multiboostOLD/build/multiboost --fileformat arff --cmatrix pendigitsTest.arff shypSingleStumpOLD.xml OUTPUTOLD.txt | grep "Testing time")
+
+TRAIN=$(echo $OLDTRAIN | awk '{print $NF}')
+TEST=$(echo $OLDTEST | awk '{print $NF}')
+echo "OLD TIMES:"
+python -c "print $TRAIN + $TEST"
 
 diff -s OUTPUT.txt OUTPUTOLD.txt
 python confusion.py
